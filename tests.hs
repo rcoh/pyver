d4f97fe84637677751ef43eb49accee6f9f69204
Russell Cohen <rcoh@mit.edu>
2013-05-03 12:43:00 -0400

    adding more tests

--- tests.hs
+++ tests.hs
@@ -14,14 +14,114 @@ makeFilebackedTest filename expected =
     code <- readFile filename
     assertEqual filename expected (evaluateCode filename code)
 
-test1 = makeTest "Simple Assignment" "x = 5\n" 
+test1 = makeFilebackedTest "basic_test.py"
   FocusedContext {left = Scope (fromList [("x",PyInt)]), right = Scope (fromList []), focus = LeftFocus}
 
 test2 = makeFilebackedTest "undefined_test.py" 
-  FocusedContext {left = Scope (fromList [("x",PyInt)]), right = Scope (fromList []), focus = LeftFocus}
+  FocusedContext {left = Scope (fromList [("x",IdentifierNotFound "a")]), right = Scope (fromList []), focus = LeftFocus}
+
+test3 = makeFilebackedTest "test_lists.py"
+    FocusedContext
+      { left = Scope (
+                 fromList
+                   [ ( "Foo"
+                     , ComplexType (
+                         fromList
+                           [ ( "__call__"
+                             , Identity
+                             )
+                           , ( "x"
+                             , PyList PyInt
+                             )
+                           , ( "y"
+                             , PyInt
+                             )
+                           ] )
+                         []
+                     )
+                   , ( "a"
+                     , PyList (
+                         UnionType
+                           [ PyBool
+                           , PyInt
+                           ] )
+                     )
+                   , ( "bad_access"
+                     , ObjectNotSubscriptable "5"
+                     )
+                   , ( "empty"
+                     , PyList Unknown
+                     )
+                   , ( "f"
+                     , ComplexType (
+                         fromList
+                           [ ( "x"
+                             , PyList PyInt
+                             )
+                           , ( "y"
+                             , PyInt
+                             )
+                           ] )
+                         []
+                     )
+                   , ( "int_list"
+                     , PyList PyInt
+                     )
+                   , ( "should_be_int1"
+                     , PyInt
+                     )
+                   , ( "should_be_int2"
+                     , PyInt
+                     )
+                   , ( "should_be_union"
+                     , UnionType
+                         [ PyBool
+                         , PyInt
+                         ]
+                     )
+                   , ( "should_be_unsub"
+                     , ObjectNotSubscriptable "f.y"
+                     )
+                   ] )
+      , right = Scope ( fromList [] )
+      , focus = LeftFocus
+      }
+
+test_nested_lists = makeFilebackedTest "test_nested_lists.py"
+    FocusedContext
+      { left = Scope (
+                 fromList
+                   [ ( "list_int_bool"
+                     , PyList (
+                         UnionType
+                           [ PyList PyBool
+                           , PyList PyInt
+                           ] )
+                     )
+                   , ( "list_list_int"
+                     , PyList ( PyList PyInt )
+                     )
+                   , ( "list_mixed_int_bool"
+                     , PyList (
+                         UnionType
+                           [ PyList (
+                               UnionType
+                                 [ PyBool
+                                 , PyInt
+                                 ] )
+                           , PyList PyInt
+                           ] )
+                     )
+                   ] )
+      , right = Scope ( fromList [] )
+      , focus = LeftFocus
+      }
 
 
-
-tests = TestList [TestLabel "test1" test1 , TestLabel "test2" test2]
+tests = TestList [
+    TestLabel "test1" test1, 
+    TestLabel "test2" test2, 
+    TestLabel "test_lists" test3,
+    TestLabel "test_nested", test_nested_lists]
 
 main = runTestTT tests
